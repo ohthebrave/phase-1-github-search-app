974fa85a9b1d490bc755559f8254714eb7028da7
George <georgejoj28@gmail.com>
2023-10-03 15:47:09 +0300

    second commit

--- js/index.js
+++ js/index.js
@@ -1,9 +1,26 @@
 
 function renderUsers(user) {
-    document.querySelector('#user-list').innerHTML = `
-              <img src="${user.avatar_url}" alt="${user.login}" />
-              <a href="${user.html_url}" target="_blank">${user.login}</a>
-            `;
+    // console.log(user)
+    let userList = document.querySelector('#user-list')
+    userList.innerHTML = `
+    <img src="${user.avatar_url}" alt="${user.login}" />
+    <div class="link">
+    <a href="${user.html_url}" target="_blank">${user.login}</a>
+    </div>
+    `;
+    userList.addEventListener('click', () => {
+        let userName = user.login
+        fetch(`https://api.github.com/users/${userName}/repos`)
+        .then(res => res.json())
+        .then(data => {
+            data.forEach(repo => {
+                document.querySelector('#repos-list').innerHTML = `
+                <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                `
+            })
+        })
+
+    })
 }
 let formInput = document.querySelector('#github-form');
 formInput.addEventListener('submit', getUsers)
